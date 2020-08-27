ce69f9a2a20886556490c7cebb901d56fd26ef22
Brian Hawkins <brian.hawkins92@gmail.com>
2020-08-27 01:30:22 -0500

    Agregado bootstrap, popper y jquery de manera estatica

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolio',
+        'USER': 'postgres',
+        'PASSWORD': '311Ab52225',
+        'HOST': 'localhost',
+        'PORT' : '5432',
     }
 }
 
@@ -119,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
